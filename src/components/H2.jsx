2bdfa01f5c3934d5de9f6fc5feeb4adcd52d8633
Gold David <davey3680@gmail.com>
2023-10-13 20:55:45 +0100

    working on project-card-row responsiveness

--- src/components/H2.jsx
+++ src/components/H2.jsx
@@ -1,9 +1,15 @@
-
-export default function H2({children}) {
+export default function H2({ children }) {
   return (
-    <div className="flex items-center">
-    <div className="bg-sec h-10 w-1.5 inline-block"></div>
-    <h2 className="text-white inline-block font-base text-base ml-3">{children}</h2>
-  </div>
-  )
+    <div>
+      <div className="hidden md:flex items-center">
+        <div className="bg-sec h-10 w-1.5 inline-block"></div>
+        <h2 className="text-white inline-block font-base text-base ml-3">
+          {children}
+        </h2>
+      </div>
+      <div>
+        <h2 className="md:hidden text-4xl font-extrabold text-white opacity-20 ">{children}</h2>
+      </div>
+    </div>
+  );
 }
