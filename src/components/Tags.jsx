52f814ffb0ebc14c633a9124573f7c4c310e2e99
Gold David <davey3680@gmail.com>
2023-10-21 21:28:10 +0100

    working on slide colors

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -6,7 +6,7 @@ export default function Tags({tags,no=3}) {
           return (
             <div
               key={tag}
-              className="text-sm font-light mb-2 mr-1 ml-0 mt-0 rounded-3xl bg-bgSec text-tert/75 py-1.5 px-4 inline-block"
+              className="text-sm font-light mb-2 mr-1 ml-0 mt-0 rounded-3xl bg-bgSec text-tert py-1.5 px-4 inline-block"
             >
               {tag}
             </div>
